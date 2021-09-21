24cb1a78de9ebf5863e18bd65bfded11dec2f79c
s117235 <s117235@student.roundrockisd.org>
2021-09-21 12:38:21 -0500

    added font + home page

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -5,22 +5,34 @@ import pic from "./pic.jpg";
 import NavBar from "./NavBar";
 import Router from "./Router";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    typography: {
+        fontFamily: [
+            'Play',
+            'sans-serif',
+        ].join(','),
+    },
+});
 
 function App() {
-  return (
-      <div className="App" style={{ backgroundImage: `url(${pic})`}}>
-          <Container maxWidth="xl" style={{ backgroundColor: '#001624', minHeight: "100vh" }}>
-              <Typography className="font-link" color='primary' variant='h1' style={{ paddingTop: ".4em" }}>
-                  <b>
-                      Start-IT: Tech Starters 
-                  </b>
-              </Typography>
-              <div>
-                  <NavBar />
-                  <Router/>
-              </div>
-          </Container>
-    </div>
+    return (
+      <ThemeProvider theme={theme}>
+          <div className="App" style={{ backgroundImage: `url(${pic})`}}>
+              <Container maxWidth="xl" style={{ backgroundColor: '#001736', minHeight: "100vh" }}>
+                  <Typography className="font-link" color='primary' variant='h1' style={{ paddingTop: ".4em" }}>
+                      <b color='primary'>
+                          Start-IT: Tech Starters 
+                      </b>
+                  </Typography>
+                  <div>
+                      <NavBar />
+                      <Router/>
+                  </div>
+              </Container>
+          </div>
+       </ThemeProvider>
   );
 }
 
--- new-app/src/Body.js
+++ new-app/src/Body.js
@@ -1,13 +1,33 @@
 import './App.css';
 import React from "react";
 import { Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
 
 function Body() {
     return (
         <>
             <Typography color="primary">
-                Welcome to the home page!
+                <h1>
+                    Welcome to Start-IT.
+                </h1>
             </Typography>
+            <Typography color="primary">
+                <h2>
+                    Use this platform to organize your project throughout the year with your teammates.
+                </h2>
+            </Typography>
+            <Card variant='outlined' maxWidth="md" style={{ flex: 1, backgroundColor: '#bd84f5' }}>
+                <CardContent>
+                    <Typography>
+                        <h1>
+                            Members
+                        </h1>
+                    </Typography>
+                    <Button style={{ color:'white' }} color="secondary" size="large" variant="contained">Add Member</Button>
+                </CardContent>
+            </Card>
         </>
     );
 }
